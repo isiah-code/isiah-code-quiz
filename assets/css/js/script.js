9a1afb1443c67c7ab8a72aee93a54227c27506a5
Isiah <feeleyisiah@gmail.com>
2021-02-10 21:18:44 -0500

    added questions

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -10,7 +10,6 @@ var progress = document.getElementById("progress");
 var scoreDiv = document.getElementById("scoreContainer");
 var button = document.getElementById("myBtn");
 
-// create our questions
 let questions = [
     {
         question : "What year did Michael Jordan join the NBA?",
@@ -30,21 +29,30 @@ let questions = [
         choiceB : "23",
         choiceC : "6",
         correct : "C"
-    }
+    },{
+        question : "What year did Michael Jordan first retire from the NBA?",
+        choiceA : "1999",
+        choiceB : "1993",
+        choiceC : "2004",
+        correct : "B"
+    },{
+        question : "What was Michael Jordan's jersey number when he competed in the Olympics?",
+        choiceA : "9",
+        choiceB : "23",
+        choiceC : "45",
+        correct : "A"
+    },
 ];
-
-// create some variables
 
 var lastQuestion = questions.length - 1;
 let runningQuestion = 0;
 let count = 0;
-var questionTime = 10; // 10s
-var gaugeWidth = 150; // 150px
+var questionTime = 10; 
+var gaugeWidth = 150; 
 var gaugeUnit = gaugeWidth / questionTime;
 let TIMER;
 let score = 0;
 
-// render a question
 function renderQuestion(){
     let q = questions[runningQuestion];
     
@@ -56,24 +64,21 @@ function renderQuestion(){
 
 button.addEventListener("click",startQuiz);
 
-// start quiz
 function startQuiz(){
     button.style.display = "none";
     renderQuestion();
     quiz.style.display = "block";
     renderProgress();
     renderCounter();
-    TIMER = setInterval(renderCounter,1000); // 1000ms = 1s
+    TIMER = setInterval(renderCounter,1000); 
 }
 
-// render progress
 function renderProgress(){
     for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
         progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
     }
 }
 
-// counter render
 
 function renderCounter(){
     if(count <= questionTime){
@@ -82,30 +87,23 @@ function renderCounter(){
         count++
     }else{
         count = 0;
-        // change progress color to red
         answerIsWrong();
         if(runningQuestion < lastQuestion){
             runningQuestion++;
             renderQuestion();
         }else{
-            // end the quiz and show the score
             clearInterval(TIMER);
             scoreRender();
         }
     }
 }
 
-// checkAnwer
 
 function checkAnswer(answer){
     if( answer == questions[runningQuestion].correct){
-        // answer is correct
         score++;
-        // change progress color to green
         answerIsCorrect();
     }else{
-        // answer is wrong
-        // change progress color to red
         answerIsWrong();
     }
     count = 0;
@@ -113,23 +111,20 @@ function checkAnswer(answer){
         runningQuestion++;
         renderQuestion();
     }else{
-        // end the quiz and show the score
         clearInterval(TIMER);
         scoreRender();
     }
 }
 
-// answer is correct
 function answerIsCorrect(){
-    document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
+    document.getElementById(runningQuestion);
 }
 
-// answer is Wrong
 function answerIsWrong(){
-    document.getElementById(runningQuestion).style.backgroundColor = "#f00";
+    document.getElementById(runningQuestion);
 }
 
-// score render
 function scoreRender(){
     scoreDiv.style.display = "block";
-    
+
+}
